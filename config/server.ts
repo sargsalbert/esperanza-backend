523e01bf53a3c6e1caf199365d531b04713e8d38
sargsalbert <sargsyan.abet@gmail.com>
2025-05-11 16:47:38 +0400

    server setting

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array(
+      "XOUzL2RAd4hRDB6xtDqfCts0E4jzeJbYyvS6pDkCgVE=,gn5l1MLHb1oE0jHjOqeS/COSn64PbO1g3D17z14KgVE="
+    ),
   },
 });
