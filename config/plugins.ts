6daf08b1fc6ced30dc3f17f40f9a0855324f60bd
sargsalbert <sargsyan.abet@gmail.com>
2025-05-12 19:08:08 +0400

    tst

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,24 +1,17 @@
-export default ({ env }: { env: (key: string) => string }) => {
-  console.log("DigitalOcean Config:", {
-    key: env("DO_SPACE_ACCESS_KEY"),
-    secret: env("DO_SPACE_SECRET_KEY"),
-    endpoint: env("DO_SPACE_ENDPOINT"),
-    space: env("DO_SPACE_BUCKET"),
-  });
-
-  return {
-    upload: {
-      config: {
-        provider: "strapi-provider-upload-do",
-        providerOptions: {
-          accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-          secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-          endpoint: env("DO_SPACE_ENDPOINT"),
-          space: env("DO_SPACE_BUCKET"),
-          //   directory: env("DO_SPACE_DIRECTORY"),
-          //     cdn: env('DO_SPACE_CDN'),
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        region: "us-east-1", // DO ignores this, just needs a valid AWS region string
+        endpoint: env("DO_SPACE_ENDPOINT"), // must include https://
+        s3ForcePathStyle: true, // required for DO Spaces compatibility
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
         },
       },
     },
-  };
-};
+  },
+});
