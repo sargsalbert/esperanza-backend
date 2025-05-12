7183ee9680a7ad43026630030e92417a7e1b9042
sargsalbert <sargsyan.abet@gmail.com>
2025-05-12 18:22:03 +0400

    strapi media config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,26 @@
-export default ({ env }: { env: (key: string) => string }) => ({
-  upload: {
-    config: {
-      provider: "aws-s3",
-      providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-        endpoint: env("DO_SPACE_ENDPOINT"),
-        region: env("DO_SPACE_REGION"),
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
+export default ({ env }: { env: (key: string) => string }) => {
+  console.log("DigitalOcean Config:", {
+    key: env("DO_SPACE_ACCESS_KEY"),
+    secret: env("DO_SPACE_SECRET_KEY"),
+    endpoint: env("DO_SPACE_ENDPOINT"),
+
+    space: env("DO_SPACE_BUCKET"),
+  });
+
+  return {
+    upload: {
+      config: {
+        provider: "strapi-provider-upload-do",
+        providerOptions: {
+          accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+          secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+          endpoint: env("DO_SPACE_ENDPOINT"),
+          space: env("DO_SPACE_BUCKET"),
+
+          //   directory: env("DO_SPACE_DIRECTORY"),
+          //     cdn: env('DO_SPACE_CDN'),
         },
       },
     },
-  },
-});
+  };
+};
