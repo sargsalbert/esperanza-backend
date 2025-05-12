d5a297eb2d76d60feb4cfb865896cfbd318ce47f
sargsalbert <sargsyan.abet@gmail.com>
2025-05-12 14:16:13 +0400

    image upload settings

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({ env }: { env: (key: string) => string }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        region: env("DO_SPACE_REGION"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+      },
+    },
+  },
+});
