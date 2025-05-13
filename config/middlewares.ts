36e78c53e7fbde58d432e361b292bbac248cad2a
sargsalbert <sargsyan.abet@gmail.com>
2025-05-13 14:46:22 +0400

    Media library config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,41 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+
+            "*.fra1.digitaloceanspaces.com",
+            "fra1.digitaloceanspaces.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+
+            "*.fra1.digitaloceanspaces.com",
+            "fra1.digitaloceanspaces.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,15 +1,16 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "@strapi/provider-upload-aws-s3",
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-        region: "us-east-1", // DO ignores this, just needs a valid AWS region string
-        endpoint: env("DO_SPACE_ENDPOINT"), // must include https://
-        s3ForcePathStyle: true, // required for DO Spaces compatibility
+        credentials: {
+          accessKeyId: env("SCALEWAY_ACCESS_KEY_ID"),
+          secretAccessKey: env("SCALEWAY_ACCESS_SECRET"),
+        },
+        region: env("SCALEWAY_REGION"),
+        endpoint: env("SCALEWAY_ENDPOINT"),
         params: {
-          Bucket: env("DO_SPACE_BUCKET"),
+          Bucket: env("SCALEWAY_BUCKET"),
         },
       },
     },
